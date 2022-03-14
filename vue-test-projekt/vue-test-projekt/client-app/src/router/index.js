97f91b1a75d6299abe60617fbff727a3bb156d73
Jo Wagner <jowagner@limago.de>
2022-03-14 13:56:42 +0100

    initial commit

---
+++ vue-test-projekt/vue-test-projekt/client-app/src/router/index.js
@@ -0,0 +1,30 @@
+﻿import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/components/Home.vue";
+import Counter from "@/components/Counter.vue";
+import FetchData from "@/components/FetchData.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    
+    {
+        path: "/FetchData",
+        name: "FetchData",
+        component: FetchData,
+    },
+{
+    path: "/Counter",
+        name: "Counter",
+        component: Counter,
+}
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
